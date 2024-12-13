c995208d5e61bd778e577e1e08890b3d383b06d1
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-12 19:39:07 -0500

    Demo: titles in constants

--- demo.py
+++ demo.py
@@ -6,6 +6,10 @@ from strath import\
 	ensure_path_is_str
 
 
+_TITLE_PATH1 = "Path 1"
+_TITLE_PATH2 = "Path 2"
+
+
 def _print_path_and_type(message, some_path):
 	print(f"{message}: {some_path} {type(some_path)}")
 
@@ -14,11 +18,11 @@ current_file1 = os.path.abspath(__file__)
 current_file2 = Path(__file__).resolve()
 
 print("Demo path")
-_print_path_and_type("Path 1", current_file1)
-_print_path_and_type("Path 2", current_file2)
+_print_path_and_type(_TITLE_PATH1, current_file1)
+_print_path_and_type(_TITLE_PATH2, current_file2)
 
 print("\nPath types swapped")
 current_file1 = ensure_path_is_pathlib(current_file1, False)
 current_file2 = ensure_path_is_str(current_file2, False)
-_print_path_and_type("Path 1", current_file1)
-_print_path_and_type("Path 2", current_file2)
+_print_path_and_type(_TITLE_PATH1, current_file1)
+_print_path_and_type(_TITLE_PATH2, current_file2)
