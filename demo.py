44a93f013243858c04c0d88236c25ad2ffe0a790
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-12 19:54:36 -0500

    Demo: tests with None

--- demo.py
+++ demo.py
@@ -14,6 +14,15 @@ def _print_path_and_type(message, some_path):
 	print(f"{message}: {some_path} {type(some_path)}")
 
 
+def _test_none_path(message, fnc, allow_none):
+	print(message)
+	try:
+		none_path = fnc(None, allow_none)
+		print(f"Return value: {none_path}")
+	except TypeError:
+		print("TypeError raised")
+
+
 current_file1 = os.path.abspath(__file__)
 current_file2 = Path(__file__).resolve()
 
@@ -26,3 +35,8 @@ current_file1 = ensure_path_is_pathlib(current_file1, False)
 current_file2 = ensure_path_is_str(current_file2, False)
 _print_path_and_type(_TITLE_PATH1, current_file1)
 _print_path_and_type(_TITLE_PATH2, current_file2)
+
+_test_none_path("\nTest with None allowed, str -> pathlib", ensure_path_is_pathlib, True)
+_test_none_path("\nTest with None disallowed, str -> pathlib", ensure_path_is_pathlib, False)
+_test_none_path("\nTest with None allowed, pathlib -> str", ensure_path_is_str, True)
+_test_none_path("\nTest with None disallowed, pathlib -> str", ensure_path_is_str, False)
