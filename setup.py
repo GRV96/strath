2a074dc93523e5aa0b989374de793e8a3487a144
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-13 20:39:31 -0500

    Version incremented to `1.0.0`, library ready for deployment

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
 	setuptools.setup(
 		name = "strath",
-		version = "0.0.0",
+		version = "1.0.0",
 		author = "Guyllaume Rousseau",
 		description = short_desc,
 		long_description = long_desc,
